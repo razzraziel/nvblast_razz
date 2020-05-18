a141dc1e15a7a91076c502afabea0732a82cb3be
razzraziel <razzraziel+git@gmail.com>
2020-05-18 12:47:57 +0300

    Some comments

--- Assets/Scripts/GameObjectGenerator.cs
+++ Assets/Scripts/GameObjectGenerator.cs
@@ -19,9 +19,13 @@ public class GameObjectGenerator : MonoBehaviour
     private void Init()
     {
         CubeAsset.Settings settings = new CubeAsset.Settings();
+        //First depth for first state, so it will be unfractred (1,1,1)
         settings.depths.Add(new CubeAsset.DepthInfo(new Vector3(1, 1, 1), NvBlastChunkDesc.Flags.NoFlags));
+        //Second depth for first fracture, so it will fractured by 2 per axis. You can add more depths or increase this depths count.
         settings.depths.Add(new CubeAsset.DepthInfo(new Vector3(2, 2, 2), NvBlastChunkDesc.Flags.SupportFlag));
+        //this is size
         settings.extents = new Vector3(10, 10, 10);
+        //this is static parts of object, so 1.0 means first row will be static.
         settings.staticHeight = 1.0f;
         _thisCubeAsset = CubeAsset.generate(settings);
         _objectToBlastFamily.Initialize(_thisCubeAsset);
